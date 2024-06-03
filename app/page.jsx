e02f3de2ff1035b0bc4809f267dd46a4df768616
SenchaMacha <protoweb88@outlook.com>
2024-06-03 09:32:31 -0700

    second

---
+++ app/page.jsx
@@ -0,0 +1,16 @@
+'use client';
+import React from 'react';
+
+const Page = () => {
+	return (
+		<div
+			initial={{}}
+			animate={{}}
+			exit={{}}
+			transition={{}}
+			className='w-screen text-5xl text-gray-600 font-semibold h-screen bg-[#dcdcdc] grid place-items-center'>
+			New International
+		</div>
+	);
+};
+export default Page;
