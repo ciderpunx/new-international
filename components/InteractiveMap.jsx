5f03610d6fe37ed8abaa90a21293c5f3d8c86703
SenchaMacha <protoweb88@outlook.com>
2024-06-09 11:59:35 -0700

    svgs added

--- components/InteractiveMap.jsx
+++ components/InteractiveMap.jsx
@@ -1,62 +1,219 @@
+"use client";
 import Image from "next/image";
 import ParaTitle from "../utils/TitlePara";
+import { useState } from "react";
+import { AnimatePresence, motion } from "framer-motion";
+import { League_Gothic, Baskervville } from "next/font/google";
+const league_gothic = League_Gothic({
+  subsets: ["latin"],
+});
+const baskerville = Baskervville({
+  subsets: ["latin"],
+  weight: ["400"],
+});
+
 const Page = () => {
+  const [mapState, setMapState] = useState({
+    hoverState: false,
+    title: " ",
+    background: "",
+    subtitle: "",
+  });
   return (
-    <div className="mx-auto flex w-full max-w-7xl flex-col gap-4 border-2">
+    <div className="relative mx-auto flex h-auto w-full max-w-7xl flex-col gap-4 border-2">
       {/* //? svg */}
-      <div className="border- relative aspect-video w-full border-violet-500">
+      <div className="relative aspect-video w-full border-violet-500">
         <div className="absolute inset-0 h-full w-full">
           <Image src="interactive-map.svg" fill="true" alt="interactive-map" />
         </div>
       </div>
       {/* //? hover elements */}
-      <div className="grid h-auto min-h-24 w-full auto-rows-[60px] grid-cols-2 gap-2 md:grid-cols-5 md:gap-4">
-        {mapData.map((x, index) => (
-          <div
+      <div className="md:auto-rows-auto-[160px] relative grid h-auto min-h-24 w-full auto-rows-[64px] grid-cols-2 gap-2 md:grid-cols-5 md:gap-4">
+        {mapInfo.map((x, index) => (
+          <motion.div
+            onTap={() => {
+              setMapState({
+                title: x.title,
+                hoverState: true,
+                background: x.background,
+                subtitle: x.subtitle,
+              });
+            }}
+            onHoverStart={() => {
+              setMapState({
+                title: x.title,
+                hoverState: true,
+                background: x.background,
+              });
+            }}
+            onHoverEnd={() => {
+              setMapState({
+                title: "",
+                hoverState: false,
+                background: "",
+              });
+            }}
             key={index}
-            className={`relative flex h-full w-full items-center rounded-sm bg-[#ebf7f7] px-1 py-2 text-sm font-semibold capitalize last:col-span-2 md:last:col-span-1`}
+            className={`relative flex h-full w-full cursor-pointer items-center rounded-sm bg-[#ebf7f7] px-1 py-2 text-sm font-semibold capitalize last:col-span-2 md:text-lg md:last:col-span-1`}
           >
-            {x.title}
-            <div
-              style={{
-                background: x.accent,
+            <span>{x.title}</span>
+            <motion.div
+              animate={{
+                height:
+                  mapState.hoverState && mapState.title == x.title
+                    ? "100%"
+                    : "8px",
               }}
-              className={`absolute bottom-0 left-0 right-0 z-40 h-2 w-full`}
-            ></div>
-          </div>
+              style={{
+                background: x.background,
+                color: x.text,
+              }}
+              className={`absolute bottom-0 left-0 right-0 grid h-2 w-full place-items-center text-lg`}
+            >
+              {mapState.hoverState && mapState.title == x.title && x.title}
+            </motion.div>
+            <AnimatePresence>
+              {mapState.hoverState && mapState.title == x.title && x.title && (
+                <motion.div
+                  initial={{ y: -10, height: 0 }}
+                  // animate={{ opacity: 1 }}
+                  animate={{ height: "40vh" }}
+                  exit={{ height: 0, opacity: 0 }}
+                  transition={{ type: "spring", bounce: 0, duration: 0.6 }}
+                  // layoutId="hover"
+                  style={{
+                    background: mapState.background,
+                    text: x.text,
+                    height: "30vh",
+                    borderBottomLeftRadius: 12,
+                    borderBottomRightRadius: 12,
+                  }}
+                  className={`${baskerville.className} absolute left-0 top-full z-40 mx-0 hidden h-[40vh] w-[25vw] flex-col items-center justify-start gap-4 divide-y-2 divide-white px-4 py-4 pt-8 text-white md:visible md:flex`}
+                >
+                  <h2 className="font-serif text-2xl">{x.subtitle}</h2>
+                  <p
+                    className={`${baskerville.className} text-balance indent-4 text-lg`}
+                  >
+                    {x.para}
+                  </p>
+                </motion.div>
+              )}
+            </AnimatePresence>
+          </motion.div>
         ))}
       </div>
       {/* //? title and para */}
+
+      <AnimatePresence>
+        {mapState.hoverState && (
+          <motion.div
+            variants={parentVariant}
+            initial="hidden"
+            animate="show"
+            exit="exit"
+            // transition={{ type: "spring", bounce: 0.9, duration: 0.8 }}
+            onClick={() => {
+              setMapState({
+                hoverState: false,
+                subtitle: "",
+                title: "",
+              });
+            }}
+            className="fixed inset-0 h-full w-full touch-none overflow-hidden bg-black/30 px-4 md:hidden"
+          >
+            {" "}
+            <motion.div
+              variants={childVariant}
+              style={{
+                color: mapState.background,
+                borderColor: mapState.background,
+              }}
+              className={`${baskerville.className} absolute bottom-0 left-0 right-0 mx-auto grid h-1/2 w-full place-items-center text-balance rounded-t-2xl border-t-8 bg-white px-2 py-4 text-center text-2xl font-semibold shadow-2xl`}
+            >
+              {mapState.subtitle}
+            </motion.div>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </div>
   );
 };
 export default Page;
 
-let mapData = [
+let parentVariant = {
+  hidden: { opacity: 0 },
+  show: {
+    opacity: 1,
+    transition: {
+      delayChildren: 0.15,
+    },
+  },
+  exit: {
+    transition: {
+      when: "afterChildren",
+    },
+  },
+};
+let childVariant = {
+  hidden: {
+    height: 0,
+  },
+  show: {
+    height: "40vh",
+    transition: {
+      duration: 0.3,
+    },
+  },
+  exit: {
+    height: 0,
+  },
+};
+
+let mapInfo = [
   {
     id: "map-one",
     title: "on request",
-    accent: "#7fbc6e",
+    background: "#7fbc6e",
+    text: "black",
+    subtitle:
+      "662 million(34%) women of reproductive age live in 77 countries that allow abortion on request.",
+    para: "The most common gestational limit for countries in this category is 12 weeks. Gestational limits are calculated from the first day of the last menstrual period, which is considered to occur two weeks prior to conception. Where laws specify that gestational age limits are calculated from the date of conception, these limits have been extended by two weeks",
   },
   {
     id: "map-two",
     title: "prohibited altogether",
-    accent: "#000000",
+    background: "#000000",
+    text: "white",
+    subtitle:
+      "457 Million (23%) women of reproductive age  live in 12 countries/territories that allow abortion on broad social or economic grounds.",
+    para: " These laws are generally interpreted liberally to permit abortion under a broad range of circumstances. These countries/territories often take into account a pregnant person’s actual or reasonably foreseeable environment and their social or economic circumstances when considering the potential impact of pregnancy and childbearing.",
   },
   {
     id: "map-three",
     title: "to preserve health",
-    accent: "#1D357D",
+    background: "#1D357D",
+    text: "white",
+    subtitle:
+      "226 Million (12%) women of reproductive age  live in 47 countries where abortion is permitted when pregnancy poses a risk to the person's health.",
+    para: "The laws of countries in this category permit abortion on the basis of health or therapeutic grounds. 25 of these countries explicitly permit abortion to preserve mental health.",
   },
   {
     id: "map-four",
     title: "to save a person's life",
-    accent: "#b61f26",
+    background: "#b61f26",
+    text: "white",
+    subtitle:
+      "416 Million (20%) women of reproductive age live in 44 countries that allow abortion to save the life of the pregnant person",
+    para: "The laws of the countries in this category permit abortion when the pregnant person’s life is at risk.",
   },
 
   {
     id: "map-five",
     title: "broad social or economic grounds",
-    accent: "#6CDCDD",
+    background: "#6CDCDD",
+    text: "black",
+    subtitle:
+      "111 Million (6%) women of reproductive age live in 21 countries that prohibit abortion altogether.",
+    para: "The laws of the countries in this category do not permit abortion under any circumstances, including when the person's life or health is at risk.",
   },
 ];
