0f581cb98a7caac2f31317d9c4ce33aa3dcaec7e
SenchaMacha <protoweb88@outlook.com>
2024-06-13 02:13:53 -0700

    svgs added

--- utils/TitlePara.jsx
+++ utils/TitlePara.jsx
@@ -13,9 +13,9 @@ const Page = ({ title, para, text20Px, textCenter }) => {
     <div className="mx-auto flex w-full max-w-5xl flex-col gap-4 md:gap-8">
       {title && (
         <h2
-          style={{
-            fontSize: 24,
-          }}
+          // style={{
+          //   fontSize: 24,
+          // }}
           className={`${league_gothic.className} ${textCenter ? "text-center" : ""} w-full text-2xl uppercase tracking-wide text-[#3c4c54] md:text-5xl`}
         >
           {title}
@@ -23,7 +23,7 @@ const Page = ({ title, para, text20Px, textCenter }) => {
       )}
       {para && (
         <p
-          className={`${libre_baskerville.className} text-left ${text20Px ? "md:text-xl" : "md:text-2xl"} indent-0 text-sm leading-7 text-[#212529] md:text-pretty md:text-xl`}
+          className={`${libre_baskerville.className} text-left ${text20Px ? "md:text-xl" : "md:text-2xl"} indent-0 text-base leading-7 text-[#212529] md:text-pretty md:text-xl`}
         >
           {para}
         </p>
