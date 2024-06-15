9b76394f1849e7e1ffc1a77b7909dfe956c2063a
SenchaMacha <protoweb88@outlook.com>
2024-06-15 01:47:03 -0700

    svgs added

--- app/layout.jsx
+++ app/layout.jsx
@@ -30,67 +30,55 @@ export default function RootLayout({ children }) {
         Fertile Ground - The Rising Anti-Abortion Movement in Ethiopia: A Threat
         to Women&apos;s Rights
       </title>
-
       <meta
         name="description"
         content="Explore the growing anti-abortion movement in Ethiopia and its impact on women's rights amidst a backdrop of social and political challenges."
       />
-
       <meta
         name="keywords"
         content="Anti-abortion movement, Ethiopia, women's rights, reproductive rights, abortion laws, healthcare, activism"
       />
-
       <meta
         name="author"
         content="Bethany Rielly, Maxine Betteridge-Moes, Maya Misikir"
       />
-
       <meta
         property="og:title"
         content="Fertile Ground - The Rising Anti-Abortion Movement in Ethiopia: A Threat to Women's Rights"
       />
-
       <meta
         property="og:description"
         content="Explore the growing anti-abortion movement in Ethiopia and its impact on women's rights amidst a backdrop of social and political challenges."
       />
-
       <meta property="og:image" content="/NewInternationalist Open Graph.png" />
-
       <meta
         property="og:url"
         content="https://newint.org/interactive/2024/fertile-ground"
       />
-
       <meta property="og:type" content="article" />
-
-      <meta property="og:site_name" content="New Internationalist Magazine" />
-
+      <meta
+        property="og:site_name"
+        content="New Internationalist Magazine"
+      />{" "}
+      <link rel="icon" href="/app/NewInt Favicon.ico" />
       <meta name="twitter:card" content="summary_large_image" />
-
       <meta
         name="twitter:title"
         content="Fertile Ground - The Rising Anti-Abortion Movement in Ethiopia: A Threat to Women's Rights"
       />
-
       <meta
         name="twitter:description"
         content="Explore the growing anti-abortion movement in Ethiopia and its impact on women's rights amidst a backdrop of social and political challenges."
       />
-
       <meta
         name="twitter:image"
         content="/NewInternationalist Open Graph.png"
       />
-
       <meta name="twitter:site" content="@newint" />
-
       <link
         rel="canonical"
         href="https://newint.org/interactive/2024/fertile-ground"
       ></link>
-
       <body className={`${inter.className} overflow-x-hidden`}>
         {children}
         <noscript>
