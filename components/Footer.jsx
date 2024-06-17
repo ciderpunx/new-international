06d4c0672b32696fdfc5d8bb2ed4c6ee59940341
SenchaMacha <protoweb88@outlook.com>
2024-06-17 05:39:31 -0700

    svgs added

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -47,10 +47,10 @@ const Page = () => {
   };
   return (
     <footer className="m-auto min-h-[120vh] w-screen bg-[#6ecad0] px-4 py-4 md:grid md:min-h-[80vh] md:place-items-center md:px-0">
-      <section className="flex h-[100dvh] w-full flex-col items-center justify-evenly gap-16 py-4 md:h-3/4 md:w-9/12 md:flex-row">
+      <section className="flex h-[110vh] w-full flex-col items-center justify-evenly gap-16 py-4 md:h-5/6 md:w-9/12 md:flex-row">
         {/* slide-1 */}
-        <div className="grid h-full w-full grid-rows-3 gap-0">
-          <div className="flex h-full w-full flex-col justify-start">
+        <div className="grid h-full w-full grid-rows-3 gap-0 bg-lime-600">
+          <div className="flex h-full w-full flex-col justify-start bg-purple-500">
             {/* //image */}
             <div className="relative m-auto h-full w-full place-self-start">
               <div className="absolute inset-0 h-full w-1/2">
@@ -65,13 +65,13 @@ const Page = () => {
           </div>
           {/* //para */}
 
-          <div className="grid h-full w-full place-items-center">
-            <p className={`${libre_baskerville.className} text-sm md:text-xl`}>
+          <div className="grid h-full w-full place-items-center bg-red-400">
+            <p className={`${libre_baskerville.className} text-sm`}>
               New Internationalist is a multi-stakeholder co-operative owned by
               its workers and approximately 4,600 co-owners.
             </p>
           </div>
-          <div className="grid h-full w-full grid-cols-[30%_70%]">
+          <div className="bg-yellw-300 grid h-full w-full grid-cols-[30%_70%]">
             <div className="relative size-16 gap-2 self-center">
               <div className="absolute inset-0 aspect-square h-full w-auto">
                 <Image src={co_op_new} fill={true} alt="co-operative" />
@@ -134,7 +134,7 @@ const Page = () => {
             Subscribe to our newsletter
           </h3>
           <div className="space-y-2">
-            <p className={`${libre_baskerville.className} text-sm md:text-xl`}>
+            <p className={`${libre_baskerville.className} text-sm md:text-sm`}>
               A weekly newsletter with the best of our journalism
             </p>
             <form
