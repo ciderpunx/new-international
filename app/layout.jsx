39a6bf56b903d02e4decdb9f6a58689809755288
SenchaMacha <protoweb88@outlook.com>
2024-06-19 12:44:46 -0700

    duplicate images removed, final touches

--- app/layout.jsx
+++ app/layout.jsx
@@ -14,18 +14,18 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <Head>
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-KD6ZVR"
-            height="0"
-            width="0"
-            className="hidden"
-            // style={{
-            //   display: "none",
-            //   visibility: "hidden",
-            // }}
-          ></iframe>
-        </noscript>
+        <script>
+          {(function (w, d, s, l, i) {
+            w[l] = w[l] || [];
+            w[l].push({ "gtm.start": new Date().getTime(), event: "gtm.js" });
+            var f = d.getElementsByTagName(s)[0],
+              j = d.createElement(s),
+              dl = l != "dataLayer" ? "&l=" + l : "";
+            j.async = true;
+            j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl;
+            f.parentNode.insertBefore(j, f);
+          })(window, document, "script", "dataLayer", "GTM-KD6ZVR")}
+        </script>
       </Head>
       <title>
         Fertile Ground - The Rising Anti-Abortion Movement in Ethiopia: A Threat
